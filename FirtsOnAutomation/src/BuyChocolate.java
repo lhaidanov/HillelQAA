eb8d9569675e891a893f4676fadda5a1bac86dd9
lhaidanov <leonid.haidanov@gmail.com>
2018-05-07 22:48:39 +0300

    add descr

--- FirtsOnAutomation/src/BuyChocolate.java
+++ FirtsOnAutomation/src/BuyChocolate.java
@@ -1,12 +1,12 @@
 public class BuyChocolate {
         public static void main(String[] args) {
-            System.out.println(countChoc(15, 1, 3));
+            System.out.println(countChoc(11, 1, 3));
         }
 
     public static int countChoc(int cash, int price, int wraps){
         int choc = cash/price; //count of choc that could be purchase by cash
         int allWraps = cash/price; //count of wraps, that could be purchase by cash
-        for (int i = allWraps; i >=3 ; i-=wraps-1) {
+        for (int i = allWraps; i >=wraps ; i-=wraps-1) {
             choc ++;
             allWraps-=wraps-1; // was changed 3 wraps to 1 choc, so was used 3 wraps and added 1 more (3-1)
         }

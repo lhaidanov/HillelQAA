c5633886ade92601c5957bd685a539fe4191b3ad
lhaidanov <leonid.haidanov@gmail.com>
2018-05-08 10:46:13 +0300

    change to double

--- FirtsOnAutomation/src/BuyChocolate.java
+++ FirtsOnAutomation/src/BuyChocolate.java
@@ -1,11 +1,11 @@
 public class BuyChocolate {
         public static void main(String[] args) {
-            System.out.println(countChoc(15, 2, 3));
+            System.out.println(countChoc(22.5, 1.5, 3));
         }
 
-    public static int countChoc(float cash, float price, int wraps){
-        float choc1 = cash/price; //count of choc that could be purchase by cash
-        float allWraps1 = cash/price; //count of wraps, that could be purchase by cash
+    public static int countChoc(double cash, double price, int wraps){
+        double choc1 = cash/price; //count of choc that could be purchase by cash
+        double allWraps1 = cash/price; //count of wraps, that could be purchase by cash
         int choc = (int)choc1;
         int allWraps = (int)allWraps1;
         for (int i = allWraps; i >=wraps ; i-=wraps-1) {

84997512d2fdc6780f1622bd199bcfde1da9045c
lhaidanov <leonid.haidanov@gmail.com>
2018-05-08 10:42:50 +0300

    change to float

--- FirtsOnAutomation/src/BuyChocolate.java
+++ FirtsOnAutomation/src/BuyChocolate.java
@@ -1,11 +1,13 @@
 public class BuyChocolate {
         public static void main(String[] args) {
-            System.out.println(countChoc(11, 1, 3));
+            System.out.println(countChoc(15, 2, 3));
         }
 
-    public static int countChoc(int cash, int price, int wraps){
-        int choc = cash/price; //count of choc that could be purchase by cash
-        int allWraps = cash/price; //count of wraps, that could be purchase by cash
+    public static int countChoc(float cash, float price, int wraps){
+        float choc1 = cash/price; //count of choc that could be purchase by cash
+        float allWraps1 = cash/price; //count of wraps, that could be purchase by cash
+        int choc = (int)choc1;
+        int allWraps = (int)allWraps1;
         for (int i = allWraps; i >=wraps ; i-=wraps-1) {
             choc ++;
             allWraps-=wraps-1; // was changed 3 wraps to 1 choc, so was used 3 wraps and added 1 more (3-1)
